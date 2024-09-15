dddd2a627e6517421f169f78b48112e5a74629a1
Chneemann <Andre-Ke@gmx.de>
2024-09-15 09:56:09 +0200

    Fix: Images were displayed black in the options menu

--- src/app/shared/components/small-btn/small-btn.component.ts
+++ src/app/shared/components/small-btn/small-btn.component.ts
@@ -13,6 +13,8 @@ export class SmallBtnComponent {
   @Input() imgSize: string = '14px';
   @Input() imgFilter: string =
     'brightness(0) saturate(100%) invert(33%) sepia(23%) saturate(5211%) hue-rotate(224deg) brightness(96%) contrast(97%)';
+  @Input() imgFilterStatic: string =
+    'brightness(0) saturate(100%) invert(0%) sepia(5%) saturate(7481%) hue-rotate(228deg) brightness(98%) contrast(106%)';
   @Input() btnSize: string = '28px';
   @Input() btnBgColor: string = '';
   @Input() btnBgHoverColor: string = '#edeefe';
